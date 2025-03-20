0a8de052751e99e11e09478b7f1d41ad36ca1d35
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-03-20 14:09:18 +0100

    HTTPS with vite 🚀

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs'
+import type { ServerOptions } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: true,
-    port: 8000
-  }
+export default defineConfig(({ mode }) => {
+    const isDev = mode === 'development';
+
+    const serverConfig: ServerOptions = {
+        host: true,
+        port: 8000,
+    };
+
+    if (isDev) {
+        serverConfig.https = {
+            key: fs.readFileSync('certs/poc-vue.key.pem'),
+            cert: fs.readFileSync('certs/poc-vue.crt.pem'),
+        };
+    }
+
+    return {
+        plugins: [vue()],
+        server: serverConfig
+    }
 })
