7f0e6399ebee33191dfc65d964467da8b7f44ef1
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-03-20 15:39:32 +0100

    views - script setup lang in ts

--- src/views/AnotherPageView.vue
+++ src/views/AnotherPageView.vue
@@ -2,10 +2,6 @@
   <Header />
   <h1>Another page</h1>
 </template>
-<script>
+<script setup lang="ts">
 import Header from "../components/Header.vue";
-
-export default {
-  components: {Header}
-}
 </script>
--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -5,10 +5,5 @@
     <router-link to="/">Back to home</router-link>
   </div>
 </template>
-<script>
-import Header from "../components/Header.vue";
-
-export default {
-  components: {Header}
-}
+<script setup lang="ts">
 </script>
